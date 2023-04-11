d9509fbd2592d3a6200fb02dc2f982e48335b0f2
furm walker <walkerfurman@yahoo.com>
2023-04-11 14:11:26 -0400

    started making nav responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,32 @@
 import './Navbar.css'
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 
 const Navbar = () => {
+
+    const [show, setShow] = useState(false)
+
+
+    const onShow = () => {
+        setShow(!show)
+        console.log('clicked')
+    }
+
     return (
         <nav className='nav-container'>
             <div className="nav-logo">
                 <h2>FW</h2>
             </div>
-            <ul>
+            <ul className={`nav-links`}>
                 <a href='#about-section'><li className="list-items">About</li></a>
                 <a href='#project-section'><li className="list-items">projects</li></a>
                 <a href='#contact-section'><li className="list-items">Contact</li></a>
             </ul>
+            <div className='hamburger'>
+                <FontAwesomeIcon icon={faBars} onClick={onShow} />
+            </div>
         </nav>
     )
 }
