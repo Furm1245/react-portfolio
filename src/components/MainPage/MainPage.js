bea1815d2b3e9effd4918dc1fc79ceb4ef9b8747
furm walker <walkerfurman@yahoo.com>
2023-04-13 09:25:59 -0400

    made about,contact, and body more reponsive

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -193,15 +193,6 @@ const MainPage = () => {
                                                 Lorem ipsum may be used as a placeholder before final copy is available.
                                                 In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. </p>
                                         </div>
-                                        {/* <div>
-                                            <ul>
-                                                <li>
-                                                </li>
-                                                <li>
-                                                </li>
-                                            </ul>
-                                            <h4>project icons</h4>
-                                        </div> */}
                                         <div>
                                             <h5>project links</h5>
                                         </div>
